1e9d138553ec9b5bd89ed59b734b8b688b08ecc3
Imran Khan <73417382+ImranKhanIMS@users.noreply.github.com>
2023-05-18 18:23:04 +0500

    getstructarray, getstructmapping, storing new element to structarry

--- src/App.js
+++ src/App.js
@@ -10,29 +10,26 @@ const greetingAddress = '0x5FbDB2315678afecb367f032d93F642f64180aa3';
 
 function App() {
 
-  // state for setting new values to the variables
-  const [message, setMessage] = useState("");
-  const [age, setAge] = useState("");
-
   const [title, setTitle] = useState("");
   const [count, setCount] = useState("");
 
-
-  // state for getting the values of the variable
-  const [n, setN] = useState("");
-  const [a, setA] = useState("");
-
   // array to store struct 'Data'
   const [data, setData] = useState([]);
+  const [dataCustom, setDataCustom] = useState([]);
 
   // array to store list of struct 'structData'
   const [dataStruct, setDataStruct] = useState([]);
-  const [dataStruct2, setDataStruct2] = useState([]);
 
-    // array to store mapping struct 'store'
-    const [store, setStore] = useState([]);
+  // Testting Array State
+  const [arr, setArr] = useState([]);
 
-  const [counter, setCounter] = useState(0);
+
+  // array to store mapping struct 'store'
+  const [store, setStore] = useState([]);
+
+  const [counter, setCounter] = useState("");
+
+  let array = [];
 
   // Helper Functions
   const requestAccount = async () => {
@@ -45,24 +42,19 @@ function App() {
       const provider = new ethers.providers.Web3Provider(window.ethereum);
       const contract = new ethers.Contract(greetingAddress, GreetingABI.abi, provider);
       try {
-        const name = await contract.getName();
-        const age = await contract.getAge();
 
         // getting struct data and storing in an array state
         const struct = await contract.getData();
+        const structUpdate = await contract.getDataCustom();
+
+        // getting struct data custom values 'structDataCustom'
         setData(struct);
+        setDataCustom(structUpdate);
         // console.log(data[0].toNumber());
 
         // getting list of struct data and storing in an array state
         const structData = await contract.getStructArray(0);
         setDataStruct(structData);
-
-        // const res = await contract.getStructArray(2);
-        // console.log(res);
-
-        // const structData2 = await contract.getDataCustom();
-        // setDataStruct2(structData2);
-        // console.log(dataStruct2);
 
         // getting mapping struct data and storing in an array state 'store'
         const store = await contract.getMapping(0);
@@ -71,79 +63,97 @@ function App() {
 
         // getting the counter value
         const counter = await contract.getCounter();
-        setCounter(counter);
+        setCounter(counter.toNumber());
+        // console.log(counter.toNumber());
 
-        setN(name);
-        setA(age.toNumber());
-        // console.log(name);
-        // console.log(age.toNumber());
+        let test;
+        for (let i=0; i<counter.toNumber(); i++) {
+
+        test = await contract.getStructArray(i);
+        array[i] = test;
+        
+        // console.log(array[2]);
+
+          // console.log(dataStruct.length);
+          // console.log(dataStruct[0].toNumber());
+          // console.log(dataStruct[1]);
+          // console.log(dataStruct[2].toNumber());
+          // console.log(dataStruct[3]);
+        }
+
+        array.map((row, i) => {
+          row.map((data, j) => {
+            console.log(data);
+          })
+        })
+        
+
+        
+
       } catch (error) {
         console.log(error);
       }
     }
   }
 
-  // Setting name of Greeting
-  const setName = async () => {
-    if (!message) return;
 
+  // Updating Custom Data of Struct 'Data'
+  const updateData = async () => {
     if (window.ethereum) {
-      const provider = new ethers.providers.Web3Provider(window.ethereum);
-      const signer = provider.getSigner();
-
-      const contract = new ethers.Contract(greetingAddress, GreetingABI.abi, signer);
-      const transaction = await contract.setName(message);
-      
-      setMessage("");
-      await transaction.wait();
-      fetchData();
-    }
-  }
-
-    const setAttributes = async () => {
-      if (!message) return;
-  
-      if (window.ethereum) {
+      try {
         const provider = new ethers.providers.Web3Provider(window.ethereum);
         const signer = provider.getSigner();
   
         const contract = new ethers.Contract(greetingAddress, GreetingABI.abi, signer);
-        if(age!="" && message!="") {
-        const transaction = await contract.setAttributes(message, age);
-          
-        setMessage("");
-        setAge("");
-        
+        const transaction = await contract.setData(title, count, true);
+  
         await transaction.wait();
         fetchData();
-      } else {
-        alert("please enter all fields!");
+      } catch (error) {
+        console.error(error);
       }
     }
-  }
+  };
 
-  // Setting ..........
-  // const pushData = async () => {
-  //   if (window.ethereum) {
-  //     try {
-  //       await window.ethereum.enable();
-  //       const provider = new ethers.providers.Web3Provider(window.ethereum);
-  //       const signer = provider.getSigner();
+  // Adding new value to list of structure 'setStructArray'
+  const pushDataArray = async () => {
+    if (window.ethereum) {
+      try {
+        const provider = new ethers.providers.Web3Provider(window.ethereum);
+        const signer = provider.getSigner();
   
-  //       const contract = new ethers.Contract(greetingAddress, GreetingABI.abi, signer);
-  //       const transaction = await contract.setData('DApp', 2, true);
+        const contract = new ethers.Contract(greetingAddress, GreetingABI.abi, signer);
+        const transaction = await contract.setStructArray(title, count, false);
   
-  //       await transaction.wait();
-  //       fetchData();
-  //     } catch (error) {
-  //       console.error(error);
-  //     }
-  //   }
-  // };
+        await transaction.wait();
+        fetchData();
+      } catch (error) {
+        console.error(error);
+      }
+    }
+  };
+
+  // Adding new value to mapping of structure 'setMapping'
+  const pushDataMapping = async () => {
+    if (window.ethereum) {
+      try {
+        const provider = new ethers.providers.Web3Provider(window.ethereum);
+        const signer = provider.getSigner();
+  
+        const contract = new ethers.Contract(greetingAddress, GreetingABI.abi, signer);
+        const transaction = await contract.setMapping(title, count, false);
+  
+        await transaction.wait();
+        fetchData();
+      } catch (error) {
+        console.error(error);
+      }
+    }
+  };
   
 
   useEffect(() => {
-    fetchData()
+    fetchData();
   }, []);
 
   return (
@@ -158,69 +168,64 @@ function App() {
           {data[0]?data[2].toNumber():''} &nbsp;
           {data[0]?data[3]?'True':'False':''}
       </span> )
-    </p>
 
-    <p> List of sturcture 'getStructArray' Values<br />
-      ( <span className='columns'>ID, Title, Count, Flag</span> )<br />
+    <br />
+
       ( <span className='values'>
-          {dataStruct[0]?(dataStruct[0]).toNumber():''} &nbsp;
-          {dataStruct[0]?dataStruct[1]:''} &nbsp;
-          {dataStruct[0]?dataStruct[2].toNumber():''} &nbsp;
-          {dataStruct[0]?dataStruct[3]?'True':'False':''}
+          {dataCustom[0]?dataCustom[0].toNumber():''} &nbsp;
+          {dataCustom[0]?dataCustom[1]:''} &nbsp;
+          {dataCustom[0]?dataCustom[2].toNumber():''} &nbsp;
+          {dataCustom[0]?dataCustom[3]?'True':'False':''}
       </span> )
     </p>
 
-    <p> List of mapping 'getMapping' Values<br />
-      ( <span className='columns'>ID, Title, Count, Flag</span> )<br />
-      ( <span className='values'>
-          {store[0]?(store[0]).toNumber():''} &nbsp;
-          {store[0]?store[1]:''} &nbsp;
-          {store[0]?store[2].toNumber():''} &nbsp;
-          {store[0]?store[3]?'True':'False':''}
-      </span> )
-    </p>
+<p> List of sturcture 'getStructArray' Values<br />
+  ( <span className='columns'>ID, Title, Count, Flag</span> )<br />
+  ( <span className='values'>
+      {dataStruct[0]?(dataStruct[0]).toNumber():''} &nbsp;
+      {dataStruct[0]?dataStruct[1]:''} &nbsp;
+      {dataStruct[0]?dataStruct[2].toNumber():''} &nbsp;
+      {dataStruct[0]?dataStruct[3]?'True':'False':''}
+  </span> )
 
-    <p>Counter: {counter!=0?counter:'0'}</p>
-{/* just to show what is typing in the box */}
-        <p>
-          {message}
-        </p>
+    <br />
+
+( <span className='values'>0, { counter }, 0</span> )
+</p>
+
+<p> List of mapping 'getMapping' Values<br />
+  ( <span className='columns'>ID, Title, Count, Flag</span> )<br />
+  ( <span className='values'>
+      {store[0]?(store[0]).toNumber():''} &nbsp;
+      {store[0]?store[1]:''} &nbsp;
+      {store[0]?store[2].toNumber():''} &nbsp;
+      {store[0]?store[3]?'True':'False':''}
+  </span> )
+</p>
+
+    <p>Counter: {counter}</p>
+
+    <br /><br />
+
+    <div className='struct'>
+
+      Struct Latest Values: &nbsp;
+      <input
+        onChange={(e) => setTitle(e.target.value)}
+        value={title} type='text' name='title' id='title' placeholder='Title' /> &nbsp;
+
+      <input
+        onChange={(e) => setCount(e.target.value)}
+        value={count} type='text' name='count' id='count' placeholder='Count' /> &nbsp;
+
+      <button className='button' onClick={updateData}>Update</button>
+      <button className='button' onClick={pushDataArray}>Add to List</button>
+      <button className='button' onClick={pushDataMapping}>Add to Mapping</button>
+    </div>
         
-        <span className='fields'>
-          <input
-            onChange={(e) => setMessage(e.target.value)}
-            value={message} type='text' name='value' id='value' placeholder='Name' />
-          &nbsp;&nbsp;&nbsp;  {n}
-        </span>
-
-        <span className='fields'>
-          <input
-            onChange={(e) => setAge(e.target.value)}
-            value={age} type='text' placeholder='Age' />
-          &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-            {a}
-        </span>
-
         <br />
-
-        <div class="struct">
-
-          arrayStruct: &nbsp;
-          <input
-            onChange={(e) => setTitle(e.target.value)}
-            value={title} type='text' name='title' id='title' placeholder='Title' /> &nbsp;
-
-          <input
-            onChange={(e) => setCount(e.target.value)}
-            value={count} type='text' name='title' id='count' placeholder='Count' /> &nbsp;
-
-          {/* <button className='button' onClick={pushData}>Push Data</button> */}
-        </div>
-        
         <div className='allButtons'>
           <button className='button' onClick={fetchData}>Fetch Struct Data</button>
-          <button className='button' onClick={setName}>Set Name</button>
-          <button className='button' onClick={setAttributes}>Set Attributes</button>
         </div>
         
       </header>
